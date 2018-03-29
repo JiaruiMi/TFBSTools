6b510757396a36fa62d0bb86695485c385f27f49
JiaruiMi <mjrpumc@outlook.com>
2018-03-29 23:23:41 +0200

    bubble plot

--- TFBSTools_and_JASPAR.R
+++ TFBSTools_and_JASPAR.R
@@ -10,7 +10,7 @@ library(JASPAR2018)
 library(Biostrings)
 
 ## Set working directory
-setwd('/Users/mijiarui/biosoft/HOMER/results/significant_alpha_vs_betaDelta_lincRNA_2')
+setwd('/Users/mijiarui/biosoft/HOMER/results/significant_delta_vs_alphaBeta_lincRNA_2')
 
 ## Prepare two file
 ### For TFBS detection, there are two files needs to be prepared. The first is a PWM (position weighted matrix)
@@ -25,12 +25,12 @@ setwd('/Users/mijiarui/biosoft/HOMER/results/significant_alpha_vs_betaDelta_linc
 ### chose other options, especially 'species' and the taxonomy_id of the species (Check it in NCBI taxonomy track)
 ?getMatrixSet
 opts = list()
-opts[['tax_group']] <- 'vertebrates'
+opts[['species']] <- 7227
 opts[['matrixtype']] <- 'PWM'
 PWMatrixList <- getMatrixSet(JASPAR2018, opts)
 
 ### Make you sequence object
-dnaSet <- readDNAStringSet('promoter_significant_alpha_vs_betaDelta_lincRNA.fa', format = 'fasta')
+dnaSet <- readDNAStringSet('promoter_significant_delta_vs_alphaBeta_lincRNA.fa', format = 'fasta')
 dnaTab <- as.data.frame(dnaSet)
 
 ### For loop to get the results. The key function is 'searchSeq()' which contains PWM and sequence 
@@ -42,13 +42,27 @@ for (i in rownames(dnaTab)){
   seqName <- i
   seqFas <- as.character(dnaTab[seqName,])
   subject <- DNAString(seqFas)
-  siteSetList <- searchSeq(PWMatrixList, subject, seqname = seqName, min.score = '90%', strand = '*')
+  siteSetList <- searchSeq(PWMatrixList, subject, seqname = seqName, min.score = '95%', strand = '*')
   if (j == 1){
-    write.table(writeGFF3(siteSetList, scoreType = 'relative'), file = 'result_vertebrates_2018_alpha.gff3', quote = F,
+    write.table(writeGFF3(siteSetList, scoreType = 'relative'), file = 'result_vertebrates_2018_delta_DM_95.gff3', quote = F,
                 sep = '\t', row.names = F)
   } else {
-    write.table(writeGFF3(siteSetList, scoreType = 'relative'), file = 'result_vertebrates_2018_alpha.gff3', quote = F,
+    write.table(writeGFF3(siteSetList, scoreType = 'relative'), file = 'result_vertebrates_2018_delta_DM_95.gff3', quote = F,
                 sep = '\t', append = T, col.names = F, row.names = F)
   }
 }
 
+
+#================================================================================
+#
+#       Bubble plot for TFBS motif prediction (from HOMER)
+#
+#================================================================================
+setwd('/Users/mijiarui/biosoft/HOMER/results')
+bubble <- read.csv('bubble_TF.csv', header = T, quote = "", check.names = F, stringsAsFactors = T)
+head(bubble)
+library('ggplot2')
+library('RColorBrewer')
+bubble$Log_normalized_counts <- as.factor(round(bubble$log_normalized_counts))
+ggplot(data = bubble, mapping = aes(x = Cell_type, y = Transcription_factor, size = Neg_logPvalue, col = log_normalized_counts)) + 
+  geom_point() + theme_classic() + scale_color_gradient(low = 'blue', high = 'red')
